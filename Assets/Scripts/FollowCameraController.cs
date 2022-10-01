60af31440d7ca6f75687becd6c7052a9a926fe62
Michal <malpa.w.klapkach@gmail.com>
2022-10-02 01:26:28 +0200

    Make follow camera separate object

--- Assets/Scripts/FollowCameraController.cs
+++ Assets/Scripts/FollowCameraController.cs
@@ -2,13 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RotationController : MonoBehaviour
+public class FollowCameraController : MonoBehaviour
 {
     [SerializeField]
     private float speed;
 
+    [SerializeField]
+    private Transform followTarget;
+
+
     private void Update()
     {
+        transform.position = followTarget.position;
+
         float horizontal = Input.GetAxis("Camera X");
         transform.Rotate(Vector3.up, horizontal * speed * Time.deltaTime);
     }
