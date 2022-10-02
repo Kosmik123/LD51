c63649c3e0f25b16f19872ae41b02e4c6ff883be
Michal <malpa.w.klapkach@gmail.com>
2022-10-02 02:06:40 +0200

    Add missing references

--- Assets/Scripts/FollowCameraController.cs
+++ Assets/Scripts/FollowCameraController.cs
@@ -7,14 +7,8 @@ public class FollowCameraController : MonoBehaviour
     [SerializeField]
     private float speed;
 
-    [SerializeField]
-    private Transform followTarget;
-
-
     private void Update()
     {
-        transform.position = followTarget.position;
-
         float horizontal = Input.GetAxis("Camera X");
         transform.Rotate(Vector3.up, horizontal * speed * Time.deltaTime);
     }
